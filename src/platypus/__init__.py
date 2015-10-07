ba84a3a453ace8b997a9850e85531966bee36ef8
Pras Velagapudi <psigen@gmail.com>
2015-10-07 17:42:00 -0400

    Added logfile read functions.

---
+++ src/platypus/__init__.py
@@ -0,0 +1,3 @@
+"""
+Platypus data analytics library.
+"""
--- src/platypus/io/logs.py
+++ src/platypus/io/logs.py
@@ -3,19 +3,20 @@
 Module for handling the import of various logfiles into numpy arrays.
 Copyright 2015. Platypus LLC. All rights reserved.
 """
+import numpy as np
 
 
 def read_v4_0_0(logfile):
     """
-    Reads text logs from v4.1.0 server.
+    Reads text logs from a Platypus vehicle server logfile.
 
     :param logfile: the logfile as an iterable
     :type  logfile: python file-like
-    :returns: a numpy recarray containing the data from this logfile.
+    :returns: a struct containing the data from this logfile.
+    :rtype: numpy.recarray
     """
     for line in logfile:
-        # TODO: fill me in!
-        pass
+        print logfile
 
 
 def load_v4_0_0(filename, *args, **kwargs):
@@ -24,8 +25,39 @@ def load_v4_0_0(filename, *args, **kwargs):
 
     :param filename: path to a log file
     :type  filename: string
-    :returns: a numpy recarray containing the data from this logfile.
+    :returns: a struct containing the data from this logfile.
+    :rtype: numpy.recarray
     """
     # At the moment, just load this one file.
     with open(filename, 'r') as logfile:
         return read_v4_0_0(logfile)
+
+
+def read(logfile, *args, **kwargs):
+    """
+    Reads text logs from a Platypus vehicle server logfile.
+
+    Attempts to auto-detect format from the file.
+
+    :param logfile: the logfile as an iterable
+    :type  logfile: python file-like
+    :returns: a struct containing the data from this logfile.
+    :rtype: numpy.recarray
+    """
+    # TODO: support multiple log types
+    return read_v4_0_0(logfile, *args, **kwargs)
+
+
+def load(filename, *args, **kwargs):
+    """
+    Loads a log from a Platypus vehicle server from a filename.
+
+    Attempts to auto-detect format from the file.
+
+    :param filename: path to a log file
+    :type  filename: string
+    :returns: a struct containing the data from this logfile.
+    :rtype: numpy.recarray
+    """
+    # TODO: support multiple log types
+    return load_v4_0_0(filename, *args, **kwargs)
