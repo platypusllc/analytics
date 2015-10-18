c0123a17d5d503a35accd5f01ca0b46e98637fc1
Pras Velagapudi <psigen@gmail.com>
2015-10-18 01:17:21 -0400

    Added comments to conversion method.

--- src/platypus/util/conversions.py
+++ src/platypus/util/conversions.py
@@ -3,7 +3,6 @@
 Module containing utility conversion functions.
 Copyright 2015. Platypus LLC. All rights reserved.
 """
-import pandas
 import utm
 
 
@@ -18,10 +17,11 @@ def add_ll_to_dataframe(df):
               [longitude, latitude], added in-place
     :rtype:   pandas.DataFrame
     """
-    # Create a conversion method to 
+    # Create a DataFrame conversion method from UTM to LL coordinates.
     def utm_to_ll(utm_row):
         return utm.to_latlon(utm_row['easting'], utm_row['northing'],
                              utm_row['zone'], northern=utm_row['hemi'])
 
+    # Apply the conversion and add the results to the original dataframe.
     df['latitude'], df['longitude'] = zip(*df.apply(utm_to_ll, axis=1))
     return df
